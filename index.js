b896756fee96234bd2663dbb63cd898d654893eb
Christopher Anderson <andersonchris227@gmail.com>
2018-08-26 19:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+      var obj = { prop: 1 }
