b3b92c545bb44639a6573d5286a9d8b6adad7025
Christopher Anderson <andersonchris227@gmail.com>
2018-08-26 19:36:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+ describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
       var obj = { prop: 1 }
+
+      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+
+      expect(obj).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+  })
